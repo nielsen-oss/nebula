032cf305604cc973b8ee238cc2a8d8cf6c9b35b1
Valerio.Maretti1 <valerio.maretti@nielsen.com>
2025-11-05 12:58:05 +0100

    TestRenameColumns

--- nlsn/tests/test_transformers/test_columns.py
+++ nlsn/tests/test_transformers/test_columns.py
@@ -6,7 +6,7 @@ import numpy as np
 import pandas as pd
 import pytest
 
-from nlsn.nebula.transformers import DropColumns, SelectColumns
+from nlsn.nebula.transformers import DropColumns, SelectColumns, RenameColumns
 from nlsn.tests.auxiliaries import get_expected_columns, from_pandas
 
 _columns_product = [f"{a}{b}" for a, b in product(["c", "d"], range(5))]
@@ -25,8 +25,8 @@ class TestDropColumns:
         [
             ("pandas", True, {"columns": ["c1", "c3"]}),
             ("polars", True, {"glob": "c*"}),
-            ("spark", False, {"regex": "c[1-5]"})
-        ]
+            ("spark", False, {"regex": "c[1-5]"}),
+        ],
     )
     def test_drop_columns(self, spark, backend, is_nw, kws):
         """Test several combinations."""
@@ -47,10 +47,70 @@ class TestDropColumns:
 
     def test_drop_columns_not_present(self):
         """Ensure DropColumns allows not existent columns by default."""
-        df_input = pd.DataFrame([[1, 2, ], [3, 4]], columns=["a", "b"])
+        df_input = pd.DataFrame(
+            [
+                [
+                    1,
+                    2,
+                ],
+                [3, 4],
+            ],
+            columns=["a", "b"],
+        )
         t = DropColumns(columns=["not_exists"])
         df_chk = t.transform(df_input)
         assert list(df_chk.columns) == df_input.columns.tolist()
+
+
+class TestRenameColumns:
+    """Test RenameColumns transformer."""
+
+    @pytest.mark.parametrize(
+        "backend, is_nw, kws, expected",
+        [
+            (
+                "pandas",
+                False,
+                {"mapping": {"c1": "new_c1", "d2": "new_d2"}},
+                ["c0", "new_c1", "c2", "c3", "c4", "d0", "d1", "new_d2", "d3", "d4"],
+            ),
+            (
+                "polars",
+                True,
+                {"columns": ["c0", "d0"], "columns_renamed": ["col_c0", "col_d0"]},
+                ["col_c0", "c1", "c2", "c3", "c4", "col_d0", "d1", "d2", "d3", "d4"],
+            ),
+            (
+                "spark",
+                True,
+                {"regex_pattern": "^c", "regex_replacement": "column_"},
+                [
+                    "column_0",
+                    "column_1",
+                    "column_2",
+                    "column_3",
+                    "column_4",
+                    "d0",
+                    "d1",
+                    "d2",
+                    "d3",
+                    "d4",
+                ],
+            ),
+        ],
+    )
+    def test_rename_columns(self, spark, backend, is_nw, kws, expected):
+        """Test several combinations."""
+        data = _get_random_int_data(5, len(_columns_product))
+        df_input = pd.DataFrame(data, columns=_columns_product)
+
+        df_input = from_pandas(df_input, backend, is_nw, spark=spark)
+
+        t = RenameColumns(**kws)
+        df_out = t.transform(df_input)
+
+        chk_cols = list(df_out.columns)
+        assert chk_cols == expected
 
 
 class TestSelectColumns:
@@ -60,9 +120,9 @@ class TestSelectColumns:
         "backend, is_nw, kws",
         [
             ("pandas", True, {"columns": "c3"}),
-            ("polars", True, {"glob": "*"}),
-            ("spark", False, {"regex": "c[23]"})
-        ]
+            ("polars", False, {"glob": "*"}),
+            ("spark", True, {"regex": "c[23]"}),
+        ],
     )
     def test_select_columns(self, spark, backend, is_nw, kws):
         """Test several combinations."""
